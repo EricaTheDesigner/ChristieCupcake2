9fee8ccc2058e037ca002195a15dbdf7dc78f3a7
EricaTheDesigner <Erica@EricaTheDesigner.com>
2018-05-02 23:58:38 -0400

    creating intents, finished linear layout

--- app/src/main/java/com/example/android/christiecupcake2/MapsActivity.java
+++ app/src/main/java/com/example/android/christiecupcake2/MapsActivity.java
@@ -1,9 +1,12 @@
 package com.example.android.christiecupcake2;
 
+import android.content.Intent;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,6 +16,8 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+
 
 /**
  * A styled map using JSON styles from a raw resource.
@@ -34,7 +39,29 @@ public class MapsActivity extends AppCompatActivity
                 (SupportMapFragment) getSupportFragmentManager()
                         .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
+
+    //method to call
+    private void dialcupcake(final String phoneNumber) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:0123456789"));
+        startActivity(intent);
+    }
+
+//method to email
+    private void emailcupcake(View view){
+    Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "I'd like to book an appointment");
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+        startActivity(intent);
+    }
+
+//    private void dialContactPhone(final String phoneNumber) {
+//        startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phoneNumber, null)));
+//    }
 
     /**
      * Manipulates the map when it's available.
@@ -57,11 +84,12 @@ public class MapsActivity extends AppCompatActivity
             Log.e(TAG, "Can't find style. Error: ", e);
         }
         // Position the map's camera near Christie Cupcake
+        // Custom icon for map
+        //zooming into street level
         LatLng cupcake = new LatLng(40.795441, -77.859011);
         googleMap.addMarker(new MarkerOptions()
                 .position(cupcake).title("Christie Cupcake at Looks")
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE)));
-
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.cupcakemapmarker)));
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cupcake, 15));
     }
 }
